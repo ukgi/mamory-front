b42da75262b59187e7bc19f0b74019bf883e9459
unknown <doosan1224@naver.com>
2022-11-19 16:20:18 +0900

    카카오맵 수정

--- mapmory/frontEnd/src/pages/map/KakaoMap.jsx
+++ mapmory/frontEnd/src/pages/map/KakaoMap.jsx
@@ -4,6 +4,7 @@ import "./map.css";
 import { Link as LinkR } from "react-router-dom";
 import EXIF from "exif-js";
 import { useEffect } from "react";
+import axios from "axios";
 
 const seoulLat = 37.5666805;
 const seoulLng = 126.9784147;
@@ -15,13 +16,31 @@ export default function KakaoMap() {
       lng: seoulLng,
     },
   });
-  const [marker, setMarker] = useState({
-    position: {
-      lat: null,
-      lng: null,
+  const [markers, setMarkers] = useState([
+    {
+      position: {
+        lat: null,
+        lng: null,
+      },
     },
-  });
+  ]);
+
+  // 📛 서버로부터 저장된 마커 데이터 가져오기
+  useEffect(() => {
+    const getMarkers = async () => {
+      try {
+        const res = await axios.get("서버로부터 저장된 마커 데이터 가져오기");
+        setMarkers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMarkers();
+  }, []);
+
+  // ✅ 사진에서 메타데이터 추출 후, 지도 위에 마커 표시하는 함수
   function uploadImgPreview() {
+    // e.preventDefault();
     const fileInfo = document.getElementById("uploadFile").files[0];
     const reader = new FileReader();
     // ✅ readAsDataURL( ) 을 통해 파일의 URL을 읽어오면 onload 실행
@@ -33,10 +52,10 @@ export default function KakaoMap() {
         var exifLatRef = EXIF.getTag(fileInfo, "GPSLatitudeRef");
 
         // 객체 내용 확인하기
-        console.dir(exifLong);
-        console.dir(exifLat);
-        console.dir(exifLongRef);
-        console.dir(exifLatRef);
+        // console.dir(exifLong);
+        // console.dir(exifLat);
+        // console.dir(exifLongRef);
+        // console.dir(exifLatRef);
 
         if (exifLatRef == "N") {
           var latitude =
@@ -55,12 +74,15 @@ export default function KakaoMap() {
         let wtmX = Math.abs(latitude);
         let wtmY = Math.abs(longitude);
 
-        setMarker({
-          position: {
-            lat: wtmX,
-            lng: wtmY,
+        setMarkers([
+          ...markers,
+          {
+            position: {
+              lat: wtmX,
+              lng: wtmY,
+            },
           },
-        });
+        ]);
         setMapCenter({
           center: {
             lat: wtmX,
@@ -68,12 +90,16 @@ export default function KakaoMap() {
           },
         });
       });
+      document.getElementById("thumbnailImg").src = reader.result;
     };
     if (fileInfo) {
       // ✅ readAsDataURL( )을 통해 파일의 URL을 읽어온다.
       reader.readAsDataURL(fileInfo);
     }
   }
+
+  const handleMarkerClick = () => {};
+
   return (
     <>
       <Map // 지도를 표시할 Container
@@ -86,21 +112,30 @@ export default function KakaoMap() {
         }}
         level={4} // 지도의 확대 레벨
       >
-        <MapMarker
-          // key={`${marker.position}-${index}`}
-          position={marker.position} // 마커를 표시할 위치
-        />
+        {markers.map((marker, index) => (
+          <MapMarker
+            key={`${marker.position}-${index}`}
+            position={marker.position} // 마커를 표시할 위치
+            onClick={handleMarkerClick}
+          />
+        ))}
       </Map>
 
-      {/* 이미지 입력 */}
-      <input
-        type='file'
-        id='uploadFile'
-        onChange={uploadImgPreview}
-        accept='image/*'
-      />
-      <br />
-      <img id='thumbnailImg' src='' width='300' />
+      {/* 이미지 및 다이어리 입력 */}
+      <form>
+        <input
+          name='image'
+          type='file'
+          id='uploadFile'
+          onChange={uploadImgPreview}
+          accept='image/*'
+        />
+        <br />
+        <img id='thumbnailImg' src='' width='300' />
+        <input type='text' placeholder='제목' name='title' />
+        <input type='text' placeholder='간단한 다이어리 작성' name='diary' />
+        <button type='submit'>등록하기</button>
+      </form>
     </>
   );
 }
